ee21d2ea075037e5c93dd46a0d0c79929666cecf
Iran Corrales Becerra <iranhoe@live.com.mx>
2019-05-01 20:19:09 -0500

    Render Layers #15

--- GameEngine2D/Game.cpp
+++ GameEngine2D/Game.cpp
@@ -21,6 +21,14 @@ std::vector<ColliderComponent*> Game::colliders;
 auto& player(manager.addEntity());
 auto& wall(manager.addEntity());
 
+enum groupLables : std::size_t
+{
+	groupMap,
+	groupPlayers,
+	groupEnemies,
+	groupColliders
+};
+
 Game::Game()
 {
 	Game::cnt = 0;
@@ -78,10 +86,12 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 	player.addComponent<KeyboardController>();
 	player.addComponent<SpriteComponent>("Assets/Sprites/Player.png");
 	player.addComponent<ColliderComponent>("player");
+	player.addGroup(groupPlayers);
 
 	wall.addComponent<TransformComponent>(300.0f, 300.0f, 300, 20, 1);
 	wall.addComponent<SpriteComponent>("Assets/Tiles/dirt.png");
 	wall.addComponent<ColliderComponent>();
+	wall.addGroup(groupMap);
 }
 
 /**
@@ -113,13 +123,29 @@ void Game::update()
 	}
 }
 
+auto& tiles(manager.getGroup(groupMap));
+auto& players(manager.getGroup(groupPlayers));
+auto& enemies(manager.getGroup(groupEnemies));
+
 /**
 
 */
 void Game::render()
 {
 	SDL_RenderClear(renderer);
-	manager.draw();
+	for (auto& t : tiles)
+	{
+		t->draw();
+	}
+	for (auto& p : players)
+	{
+		p->draw();
+	}
+	for (auto& e : enemies)
+	{
+		e->draw();
+	}
+
 	SDL_RenderPresent(renderer);
 }
 
@@ -138,5 +164,6 @@ void Game::AddTile(int id, int x, int y)
 {
 	auto& tile(manager.addEntity());
 	tile.addComponent<TileComponent>(x, y, 32, 32, id);
+	tile.addGroup(groupMap);
 }
 
